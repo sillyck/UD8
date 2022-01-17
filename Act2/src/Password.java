046e5b7a1e98c49a493bc46b07ab16867e49a311
Jordi Ribellas <ribellas2002@gmail.com>
2022-01-17 19:48:16 +0100

    Commit2

--- Act2/src/Password.java
+++ Act2/src/Password.java
@@ -1,17 +1,34 @@
 
 public class Password {
 		
-		private int longitud = 8;
+		private int longitud;
 		private String contraseña;
 		
+		public Password() {
+			this.longitud = 8;
+			this.contraseña = generarContraseña(longitud);
+		}
+
 		public Password(int longitud, String contraseña) {
 			
 			this.longitud = longitud;
-			this.contraseña = contraseña;
+			this.contraseña = generarContraseña(longitud);
 		}
 		
-		
-		
-		
-		
+	    public String getContraseña() {
+			return contraseña;
+		}
+
+		public static String generarContraseña(int l) {
+	    	int contra;
+	    	String contraS = "", rFinal="";
+	    	
+	    	for (int i = 0;i<l;i++) {
+	    		contra = (int) (Math.random()* (l - 1));
+	    		contraS = String.valueOf(contra);
+	    		rFinal = rFinal + contraS;
+	    	}
+	    	
+	    	return rFinal; 
+	    }
 }
---
+++ Act2/src/PasswordApp.java
@@ -0,0 +1,10 @@
+
+public class PasswordApp {
+
+	public static void main(String[] args) {
+		
+		Password Contra1 = new Password();
+		System.out.println(Contra1.getContraseña());
+	}
+
+}
