09f25ac40d695fc026921a300628298a6587f00c
Jordi Ribellas <ribellas2002@gmail.com>
2022-01-17 21:15:01 +0100

    Commit

---
+++ Act3/src/Electrodomestico.java
@@ -0,0 +1,31 @@
+
+public class Electrodomestico {
+
+	protected double precioBase=0;
+	protected String color="";
+	protected char conElec=' ';
+	protected double peso=0;
+	
+	public Electrodomestico() {
+		this.color="blanco";
+		this.conElec='F';
+		this.precioBase=100;
+		this.peso=5;
+	}
+
+	public Electrodomestico(double precioBase, double peso) {
+		this.color="blanco";
+		this.conElec='F';
+		this.precioBase = precioBase;
+		this.peso = peso;
+	}
+
+	public Electrodomestico(double precioBase, String color, char conElec, double peso) {
+		this.precioBase = precioBase;
+		this.color = color;
+		this.conElec = conElec;
+		this.peso = peso;
+	}
+	
+	
+}
---
+++ Act3/src/ElectrodomesticoApp.java
@@ -0,0 +1,10 @@
+
+public class ElectrodomesticoApp {
+
+	public static void main(String[] args) {
+		
+		Electrodomestico e1 = new Electrodomestico(200.00, 50.00);
+
+	}
+
+}
